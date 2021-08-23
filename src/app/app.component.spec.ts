f423c98c4176050280ea64db5fcbafdd1e5d3c59
keertana1016 <keertanasreenivasan@gmail.com>
2021-08-23 18:04:33 +0530

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('squashapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
   });
 });
